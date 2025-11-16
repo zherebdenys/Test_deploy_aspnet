ec762fa9fb37c3fa36a08a319c7319ad6bd54aa5
Denys Zhereb <d.zhereb@replyco.com>
2025-11-16 17:13:09 +0200

    commit

--- UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d41e34cd4e795346d8598066593f7955e7ca7e0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d69cf438bc1f223764cd22bcbfcde468ac1954cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
