7f7b446eb6244ae84e4216e3cf32ccc3d96bc53f
Denys Zhereb <d.zhereb@replyco.com>
2025-11-17 12:02:28 +0200

    removed bad test

--- UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d69cf438bc1f223764cd22bcbfcde468ac1954cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b2bf4c424c4a138ad4e24895b7332c188b6eaf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
